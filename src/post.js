07c311ce51a195e5c7b9e8909a297fda0c6fd98a
Steven Robertson <steverobertson@gmail.com>
2022-02-17 22:52:35 +0000

    Removed redundant commented-out code from file

--- src/post.js
+++ src/post.js
@@ -1,5 +1,5 @@
 Module['postRun'] = [];
 Module['postRun'].push(function() {
-  var output = FS.readFile('output.tap'/*, { encoding: 'utf8' }*/);
+  var output = FS.readFile('output.tap');
   Module['resolve'](output);
 });
